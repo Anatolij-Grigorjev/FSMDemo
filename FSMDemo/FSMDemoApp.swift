ad3c53a2274cb9c07d45fa16102630a8616c6e5e
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2024-03-30 16:57:02 +0200

    Initial Commit

---
+++ FSMDemo/FSMDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FSMDemoApp.swift
+//  FSMDemo
+//
+//  Created by Anatolij Grigorjev on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct FSMDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
